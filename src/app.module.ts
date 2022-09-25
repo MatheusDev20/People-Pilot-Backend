db33335254217184be5234813fb1c8f8b6090ffd
Matheus de Paula <matheusdev20@gmail.com>
2022-09-25 18:37:02 -0300

    feat: db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
 bootstrap();
