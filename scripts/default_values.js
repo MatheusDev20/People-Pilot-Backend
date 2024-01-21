ee94a765bec5ed6f7c3e930be165fea7e4f9f88f
matheus <matheusdev20@gmail.com>
2024-01-21 18:15:54 -0300

    feat: better implementation of the default insert script

--- scripts/default_values.js
+++ scripts/default_values.js
@@ -3,10 +3,10 @@
 
 const typeorm = require('typeorm');
 const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
 const { DataSource } = typeorm;
 
 const entities = ['../dist/modules/**/entities/*.entity.js'];
-
 const source = new DataSource({
   type: 'mysql',
   host: 'localhost',
@@ -16,6 +16,10 @@ const source = new DataSource({
   database: 'ksxbackend',
   entities,
 });
+
+const SALT_ROUNDS = 12;
+const defaultUserMail = 'admin@dunder.com';
+const defaultPass = bcrypt.hash('admin123456', SALT_ROUNDS);
 
 async function run() {
   try {
@@ -30,13 +34,14 @@ async function run() {
     );
     await source.query(
       `INSERT INTO employee (id, name, email, password, phone, birthDate, hire_date, position, updated_at, created_at, status, departmentId, roleId)
-      VALUES ("${uuidv4()}", "Admin", "admin@dunder.com", "admin123", "32999850138", "1999-01-01", NOW(), "SYSTEM Admin", NOW(), NOW(), "Active", "${defaultDepartmentId}", 1)`,
+      VALUES ("${uuidv4()}", "Admin", "${defaultUserMail}", "${defaultPass}", "32999850138", "1999-01-01", NOW(), "SYSTEM Admin", NOW(), NOW(), "Active", "${defaultDepartmentId}", 1)`,
     );
     await source.query(
       'INSERT INTO banks (id, name, code) VALUES (1, "Itaú Unibanco S.A.", 341)',
     );
   } catch (err) {
-    console.log(err);
+    console.log('Error inserting values', err.message);
+    process.exit(1);
   } finally {
     await source.destroy();
     process.exit();
