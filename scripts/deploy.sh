6013e262aec354255ae852354bfdfa8fddaafaff
matheus <matheusdev20@gmail.com>
2024-03-30 14:20:47 -0300

    new deploy varis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,12 +2,14 @@ echo 'Starting Deploy...'
 
 echo 'Cloning the repository...'
 
+cd /var/www && rm -rf /api
+
 git clone https://github.com/MatheusDev20/People-Pilot-Backend.git /var/www/api
 
 cd /var/www/api
 
 echo 'Setup Enviroment Variables...'
-chmod +x ./env-vars.sh && ./env-vars.sh
+cd ~ && chmod +x ./env-vars.sh && ./env-vars.sh
 
 echo 'Installing dependencies...'
 
