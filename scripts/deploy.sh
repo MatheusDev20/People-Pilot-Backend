9e9920e3170cd93ac0e6c2571c7809b07a5a45ee
matheus <matheusdev20@gmail.com>
2024-03-30 14:27:33 -0300

    new version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,16 +2,18 @@ echo 'Starting Deploy...'
 
 echo 'Cloning the repository...'
 
-cd /var/www && rm -rf /api
+cd /var/www 
+rm -rf /api
 
 git clone https://github.com/MatheusDev20/People-Pilot-Backend.git /var/www/api
 
-cd /var/www/api
-
 echo 'Setup Enviroment Variables...'
-cd ~ && chmod +x ./env-vars.sh && ./env-vars.sh
+chmod +x ~/env-vars.sh
+~/env-vars.sh
 
 echo 'Installing dependencies...'
+
+cd /var/www/api
 
 node -v && \
 yarn && \
